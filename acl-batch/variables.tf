b1310f4e086a546a52eceb669fca8dd3491175d9
Max Levine <max.levine@vivacitylabs.com>
2024-02-01 22:37:34 +0000

    fix: Alphabetise sub module vars

--- acl-batch/variables.tf
+++ acl-batch/variables.tf
@@ -1,3 +1,53 @@
+variable "acl_host" {
+  type        = string
+  description = "Host from which principal listed in acl_principal will have access."
+}
+
+variable "acl_operation" {
+  type        = string
+  description = "Operation that is being allowed or denied. Valid values: Unknown, Any, All, Read, Write, Create, Delete, Alter, Describe, ClusterAction, DescribeConfigs, AlterConfigs, IdempotentWrite."
+  validation {
+    condition     = contains(["Unknown", "Any", "All", "Read", "Write", "Create", "Delete", "Alter", "Describe", "ClusterAction", "DescribeConfigs", "AlterConfigs", "IdempotentWrite"], var.acl_operation)
+    error_message = "Valid values: Unknown, Any, All, Read, Write, Create, Delete, Alter, Describe, ClusterAction, DescribeConfigs, AlterConfigs, IdempotentWrite."
+  }
+}
+
+variable "acl_permission_type" {
+  type        = string
+  description = "Type of permission. Valid values are Unknown, Any, Allow, Deny."
+  validation {
+    condition     = contains(["Unknown", "Any", "Allow", "Deny"], var.acl_permission_type)
+    error_message = "Valid values: Unknown, Any, Allow, Deny"
+  }
+}
+
+variable "acl_principal_prefix" {
+  type        = string
+  description = "Prefix for the ACL principal. Principal is constructed as $${var.acl_principal_prefix}$${local.offset + count.index}$${acl_principal_suffix}"
+}
+
+variable "acl_principal_suffix" {
+  type        = string
+  description = "Suffix for the ACL principal. Principal is constructed as $${var.acl_principal_prefix}$${local.offset + count.index}$${acl_principal_suffix}"
+}
+
+variable "acls_per_batch" {
+  type = number
+  validation {
+    condition     = var.acls_per_batch > 0 && var.acls_per_batch <= 5000
+    error_message = "0 < acls_per_batch <= 5000; Higher numbers will be super slow to generate the tf state graph. Put a `count` argument on an instantiation of this module instead."
+  }
+}
+
+variable "index" {
+  type        = number
+  description = "The index of this module instantiation. Assumes multiple copies of this module will be created using the count argument."
+  validation {
+    condition     = var.index >= 0
+    error_message = "Index must be >= 0."
+  }
+}
+
 variable "max_acl" {
   type        = number
   description = "The id of the highest ACL to apply. ACLs will be created up to and including this id."
@@ -12,39 +62,12 @@ variable "resource_name" {
   description = "The name of the resource being applied. When applying topics (the default), this is the topic name."
 }
 
-variable "acl_principal_prefix" {
+variable "resource_pattern_type_filter" {
   type        = string
-  description = "Prefix for the ACL principal. Principal is constructed as $${var.acl_principal_prefix}$${local.offset + count.index}$${acl_principal_suffix}"
-}
-
-variable "acl_principal_suffix" {
-  type        = string
-  description = "Suffix for the ACL principal. Principal is constructed as $${var.acl_principal_prefix}$${local.offset + count.index}$${acl_principal_suffix}"
-}
-
-variable "index" {
-  type        = number
-  description = "The index of this module instantiation. Assumes multiple copies of this module will be created using the count argument."
+  description = "The pattern filter. Valid values are Prefixed, Any, Match, Literal"
   validation {
-    condition     = var.index >= 0
-    error_message = "Index must be >= 0."
-  }
-}
-
-variable "acls_per_batch" {
-  type = number
-  validation {
-    condition     = var.acls_per_batch > 0 && var.acls_per_batch <= 5000
-    error_message = "0 < acls_per_batch <= 5000; Higher numbers will be super slow to generate the tf state graph. Put a `count` argument on an instantiation of this module instead."
-  }
-}
-
-variable "acl_operation" {
-  type        = string
-  description = "Operation that is being allowed or denied. Valid values: Unknown, Any, All, Read, Write, Create, Delete, Alter, Describe, ClusterAction, DescribeConfigs, AlterConfigs, IdempotentWrite."
-  validation {
-    condition     = contains(["Unknown", "Any", "All", "Read", "Write", "Create", "Delete", "Alter", "Describe", "ClusterAction", "DescribeConfigs", "AlterConfigs", "IdempotentWrite"], var.acl_operation)
-    error_message = "Valid values: Unknown, Any, All, Read, Write, Create, Delete, Alter, Describe, ClusterAction, DescribeConfigs, AlterConfigs, IdempotentWrite."
+    condition     = contains(["Prefixed", "Any", "Match", "Literal"], var.resource_pattern_type_filter)
+    error_message = "Valid values: Prefixed, Any, Match, Literal."
   }
 }
 
@@ -54,28 +77,5 @@ variable "resource_type" {
   validation {
     condition     = contains(["Unknown", "Any", "Topic", "Group", "Cluster", "TransactionalID"], var.resource_type)
     error_message = "Valid values: Unknown, Any, Topic, Group, Cluster, TransactionalID."
-  }
-}
-
-variable "acl_host" {
-  type        = string
-  description = "Host from which principal listed in acl_principal will have access."
-}
-
-variable "acl_permission_type" {
-  type        = string
-  description = "Type of permission. Valid values are Unknown, Any, Allow, Deny."
-  validation {
-    condition     = contains(["Unknown", "Any", "Allow", "Deny"], var.acl_permission_type)
-    error_message = "Valid values: Unknown, Any, Allow, Deny"
-  }
-}
-
-variable "resource_pattern_type_filter" {
-  type        = string
-  description = "The pattern filter. Valid values are Prefixed, Any, Match, Literal"
-  validation {
-    condition     = contains(["Prefixed", "Any", "Match", "Literal"], var.resource_pattern_type_filter)
-    error_message = "Valid values: Prefixed, Any, Match, Literal."
   }
 }
