eec3c4ac15fcf6e521b61d743f3059478fb85651
Caspar Wylie <casparwylie@gmail.com>
2025-03-24 15:24:23 +0000

    Use explicit VP acl setting seperate to batching (#9)
    * Use explicit VP acl setting seperate to batching
    
    * Default batch max 0

--- acl-batch/variables.tf
+++ acl-batch/variables.tf
@@ -95,9 +95,3 @@ variable "excluded_ids" {
   description = "Do not create ACLs for ids passed into this list"
   default     = []
 }
-
-variable "explicit_ids" {
-  type        = list(number)
-  description = "Include these IDs regardless of bulk iteration"
-  default     = []
-}
